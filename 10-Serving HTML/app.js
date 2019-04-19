6c340d5b6005c23a5b941df0ace381658d19b2c4
Dinush Chathurya <38061436+dinushchathurya@users.noreply.github.com>
2019-04-19 11:08:32 +0530

    Add files via upload

---
+++ 10-Serving HTML/app.js
@@ -0,0 +1,12 @@
+var http = require ('http');
+var fs = require ('fs');
+
+var server =http.createServer(function(req,res){
+  console.log('Request was made: ' + req.url);
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html' ,'UTF-8');
+  myReadStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log ('You are listening to port 3000. To see the output type 127.0.0.1:3000 on your broweser.');
